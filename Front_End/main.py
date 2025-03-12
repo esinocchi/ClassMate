6a38c53d9e8889a4b0e4e30bc31d4aa65276d5ae
Jacob-Meert <jacobameert@gmail.com>
2025-03-12 15:19:57 -0400

    fixed conflict with nginx CORS

--- Front_End/main.py
+++ Front_End/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["https://psu.instructure.com"],            # Domains allowed to access your API
-    allow_credentials=True,
-    allow_methods=["*"],              # Allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],              # Allow all headers
-)
 
 @app.get('/')
 async def root():
